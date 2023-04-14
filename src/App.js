d84d7c10ff24d8f42ff482fdca028c278135e203
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-04-14 22:32:56 +0300

    Components:example reactive components and use state

--- src/App.js
+++ src/App.js
@@ -1,138 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
-function MyButton() {
-  return (
-      <button>
-        I'm a button
-      </button>
-  );
-}
 
-function AboutPage() {
+function FilterableProductTable({ products }) {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
     return (
-        <>
-            <h1>About</h1>
-            <p>Hello there.<br />How do you do?</p>
-        </>
-    );
-}
-function AdminPanel() {
-    return (
-        <>
-            <h1>AdminPanel</h1>
-            <p>Hello there.<br />You are admin</p>
-        </>
-    );
-}
-function LoginForm() {
-    return (
-        <>
-            <h1>LoginForm</h1>
-            <p>Hello there.<br />Please login</p>
-        </>
+        <div>
+            <SearchBar
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly} />
+            <ProductTable
+                products={products}
+                filterText={filterText}
+                inStockOnly={inStockOnly} />
+        </div>
     );
 }
 
-const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
-};
-
-let content;
-if (false) {
-    content = <AdminPanel />;
-} else {
-    content = <LoginForm />;
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    );
 }
-const products = [
-    { title: 'Cabbage', id: 1 },
-    { title: 'Garlic', id: 2 },
-    { title: 'Apple', id: 3 },
+
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name :
+        <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if (
+            product.name.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) === -1
+        ) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category} />
+            );
+        }
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name} />
+        );
+        lastCategory = product.category;
+    });
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function SearchBar({
+                       filterText,
+                       inStockOnly,
+                       onFilterTextChange,
+                       onInStockOnlyChange
+                   }) {
+    return (
+        <form>
+            <input
+                type="text"
+                value={filterText} placeholder="Search..."
+                onChange={(e) => onFilterTextChange(e.target.value)} />
+            <label>
+                <input
+                    type="checkbox"
+                    checked={inStockOnly}
+                    onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
 ];
 
-const listItems = products.map(product =>
-    <li key={product.id}>
-        {product.title}
-    </li>
-
-);
-
-function MyButton3() {
-    const [count, setCount] = useState(0);
-
-    function handleClick() {
-        setCount(count + 1);
-    }
-
-    return (
-        <button onClick={handleClick}>
-            Clicked {count} times
-        </button>
-    );
+export default function App() {
+    return <FilterableProductTable products={PRODUCTS} />;
 }
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MyButton/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <AboutPage />
-        <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg" alt = "alt"/>
-
-        <div >
-        <h1>{user.name}</h1>
-        <img
-            className="avatar"
-            src={user.imageUrl}
-            alt={'Photo of ' + user.name}
-            style={{
-                width: user.imageSize,
-                height: user.imageSize
-            }}
-        />
-            </div >
-        <div>
-            {content}
-        </div>
-        <div>
-            {true && <AdminPanel />}
-        </div>
-
-<div>
-    <ul>{listItems}</ul>
-    <ul> <li >
-        list item just a test
-    </li></ul>
-
-</div>
-
-        <div>
-            <h1>Counters that update separately</h1>
-            <MyButton3 />
-            <MyButton3 />
-        </div>
-
-    </div>
-  );
-
-}
-
-export default App;
