99cf498b324a727b08ec65482d6ce5795388142b
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-04-14 13:45:48 +0300

    Components:add MyButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+function MyButton() {
+  return (
+      <button>
+        I'm a button
+      </button>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MyButton/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
