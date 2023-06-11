87d9fbe57e99dc67c81c9a552131e6143818dab7
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-06-11 20:05:32 +0300

    Components : create draft Task component with fetch in separate file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+import React, { useEffect, useState } from "react";
+import Tasks from './Tasks';
 function FilterableProductTable({ products }) {
     const [filterText, setFilterText] = useState('');
     const [inStockOnly, setInStockOnly] = useState(false);
@@ -171,28 +171,48 @@ const PRODUCTS = [
 ];
 
 
+//
+// const TASKS = [{
+//         "id": "5820a4c2abe74f409da89217bf905a0c",
+//         "description": "Read a\n Book",
+//         "created": "2018-09-02T16:00:01",
+//         "modified": "2018-09-02T16:00:01",
+//         "completed": false
+//     },
+//     {
+//         "id": "7fd921cfd2b64dc7b995633e8209f385",
+//         "description": "Buy\n Milk",
+//         "created": "2018-09-23T15:00:01",
+//         "modified": "2018-09-23T15:00:01",
+//         "completed": false
+//     },
+//     {
+//         "id": "a44b6db26aef49e39d1b68622f55c347",
+//         "description": "Go to Spring\n One 2018",
+//         "created": "2018-09-18T12:00:00",
+//         "modified": "2018-09-18T12:00:00",
+//         "completed": true
+//     }];
+async function fetchTasks() {
+    const response = await fetch('http://laptop-mm38guqj:8080/api/todo', {
+       // mode: 'no-cors',//needs for crossdaomain
+        mode:        'cors',
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization':'Basic dXNlcjpwYXNzd29yZA=='
+        }
+    });
+    // waits until the request completes...
+    console.log(response);
+    const tasks = await response.json();
+    return tasks;
 
-const TASKS = [{
-        "id": "5820a4c2abe74f409da89217bf905a0c",
-        "description": "Read a\n Book",
-        "created": "2018-09-02T16:00:01",
-        "modified": "2018-09-02T16:00:01",
-        "completed": false
-    },
-    {
-        "id": "7fd921cfd2b64dc7b995633e8209f385",
-        "description": "Buy\n Milk",
-        "created": "2018-09-23T15:00:01",
-        "modified": "2018-09-23T15:00:01",
-        "completed": false
-    },
-    {
-        "id": "a44b6db26aef49e39d1b68622f55c347",
-        "description": "Go to Spring\n One 2018",
-        "created": "2018-09-18T12:00:00",
-        "modified": "2018-09-18T12:00:00",
-        "completed": true
-    }];
+
+
+
+}
+
 function TaskPane({ tasks }) {
     const [filterText, setFilterText] = useState('');
     const [activeOnly, setActiveOnly] = useState(false);
@@ -214,12 +234,20 @@ function TaskPane({ tasks }) {
 }
 
 function MainFrame(){
+    // let TASKS;
+    // (async () => {
+    //     TASKS =  fetchTasks().then(tasks =>{tasks;});
+    //
+    // })();
+
+ //todo find proper place
     return <div>
        <div><FilterableProductTable products={PRODUCTS}/></div>
 <div>_____________________________________________________</div>
-        <div><TaskPane tasks={TASKS} /></div>
+
+        <Tasks/>
     </div>
-};
-export default function App() {
+}
+export default function App2() {
     return <MainFrame />;
 }
---
+++ src/Tasks.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react"
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([])
+
+    const fetchTaskData = () => {
+        fetch('http://laptop-mm38guqj:8080/api/todo', {
+            // mode: 'no-cors',//needs for crossdaomain
+            mode:        'cors',
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization':'Basic dXNlcjpwYXNzd29yZA=='
+            },
+        }).then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setTasks(data)
+            })
+    }
+
+    useEffect(() => {
+        fetchTaskData()
+    }, [])
+
+    return (
+        <div>
+            {tasks.length > 0 && (
+                <ul>
+                    {tasks.map(task => (
+                        <li key={task.id}>{task.description}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default Tasks;
