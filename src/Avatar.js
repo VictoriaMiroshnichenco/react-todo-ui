4cad93667cc1fff1147fe5f003267ce48f4854dc
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-08-08 19:45:38 +0300

    Components : Avatar

---
+++ src/Avatar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Avatar() {
+    return (<div  >
+            <h1 style={{ color: 'red' }}>User Profile</h1>
+            <h1 className="avatar">Raichel Zane</h1>
+        <img className="avatar" src="https://i.imgur.com/b8JVvXW.jpg" title="source: imgur.com" />
+        </div>
+    );
+}
+export default Avatar;
---
+++ src/Profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Profile() {
+  return (<div  >
+        <h1 style={{ color: 'red' }}>User Profile</h1>
+         </div>
+    );
+}
+export default Profile;
