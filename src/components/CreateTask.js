ce4800adf1c75ac87eee2aeddc3a8d1adfc2de2f
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-08-09 21:46:15 +0300

    Components:Create Task- works

---
+++ src/components/CreateTask.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+function CreateTask() {
+   const [editForm, setEditForm] = useState({
+        id: "",
+        description: "",
+        taskStatus:""
+
+    });
+   const [message,setMessage] = useState('');
+    let {id, description, taskStatus} = editForm;
+
+    function handleChange(e) {
+        setEditForm({
+            ...editForm,
+            [e.target.name]: e.target.value
+        })
+    };
+    function handleEditForm(e) {
+        e.preventDefault();
+        fetch(`http://laptop-mm38guqj:8080/api/todo/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization':'Basic dXNlcjpwYXNzd29yZA=='
+            },
+            body: JSON.stringify(editForm),
+        }).then(resp => {console.log("Created")})
+            .then(updatedCustomer => {
+                handleCustomerUpdate(updatedCustomer)})
+    };
+    function handleChange(e) {
+        setEditForm({
+            ...editForm,
+            [e.target.name]: e.target.value
+        })
+    };
+    function handleCustomerUpdate(updatedCustomer) {
+        setMessage("Done!");
+      //  onUpdateCustomer(updatedCustomer);
+        console.log("handleCustomerUpdate")
+    }
+
+    return (<div  >
+            <h1 style={{ color: 'red' }}>Create Task</h1>
+            <form onSubmit={handleEditForm}>
+                <input type="text" name="description" value={description} onChange={handleChange}/>
+                <input type="text" name="taskStatus" value={taskStatus} onChange={handleChange}/>
+                <button type="submit">Submit Changes</button>
+            </form>
+            <h5>{message}</h5>
+            </div>
+    );
+}
+export default CreateTask;
