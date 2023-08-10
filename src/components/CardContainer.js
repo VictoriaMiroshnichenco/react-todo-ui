9e53bd4e2b2045add7d7a2a31305166dda101021
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-08-10 19:41:10 +0300

    Components:Task card container render in loop

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,16 +1,20 @@
 import React from "react";
 import CardTaskTemplate from './CardTaskTemplate';
 function CardContainer({tasks,status}) {
-   
-    let lgh = status;
+
+
     return (<div  >
             <h1 style={{ color: 'red' }}>CardContainer</h1>
             <div className="cardContainer" id={status}>
-          <p className="nameColumns">status {lgh}</p>
-                <div><CardTaskTemplate task={tasks[0]}/></div>
-                <div><CardTaskTemplate task={tasks[1]}/></div>
-                <div><CardTaskTemplate task={tasks[2]}/></div>
+          <p className="nameColumns">Status {status}</p>
+                {/*<div><CardTaskTemplate task={tasks[0]}/></div>*/}
+                {/*<div><CardTaskTemplate task={tasks[1]}/></div>*/}
+                {/*<div><CardTaskTemplate task={tasks[2]}/></div>*/}
 
+
+                {tasks.map((tsk) => (
+                    <CardTaskTemplate task={tsk}></CardTaskTemplate>
+                ))}
             </div>
       
         </div>
